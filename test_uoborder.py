fac53ceaab587ce865e655e58029835f6ce2d199
Lambokini <lambokini888@gmail.com>
2013-12-30 16:32:32 +0800

    add  subpackage Order

---
+++ test_uoborder.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+#  Copyright (c) 2013, Corey Goldberg (cgoldberg@gmail.com)
+#
+#  license: GNU LGPL
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  Requires: Python 2.7/3.2+
+
+
+from Order.uoc import  *
+
+
+
+if __name__ == '__main__':
+	print "s"
+	print UOB_USERNAME
+	print UOB_PASSWORD
